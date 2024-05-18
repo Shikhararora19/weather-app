7cdae8dbae00f30910de8b739ca08bff3e032d51
Shikhararora19 <shikhar3@ualberta.ca>
2024-05-18 14:46:10 -0600

    Defining function and adding event listners

---
+++ index.js
@@ -0,0 +1,37 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelectorAll(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "87da805b6b8b3c0ad26af9cd3585cc13";
+
+
+weatherForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    const city = cityInput.value;
+    if (city){
+    getWeatherData(city);
+    }
+    else{
+        displayError("Please enter a city name");
+    
+    }
+    cityInput.value = "";
+})
+
+async function getWeatherData(city){
+
+}
+
+function displaWeatherInfo(data){
+
+
+}
+
+function getWeatherEmoji(weatherID){
+
+}
+
+function displayError(message){
+
+
+}
