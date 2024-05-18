98f66b1178841c1a8908854a872509f6931e9684
Shikhararora19 <shikhar3@ualberta.ca>
2024-05-18 15:24:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,39 @@ const card = document.querySelector(".card");
 const apiKey = "87da805b6b8b3c0ad26af9cd3585cc13";
 
 
-weatherForm.addEventListener("submit", (e) => {
+weatherForm.addEventListener("submit", async (e) => {
 
     e.preventDefault();
     const city = cityInput.value;
     if(city){
-        getWeatherData(city);
+        try{
+        const weatherData = await getWeatherData(city);
+        displaWeatherInfo(weatherData);
     }
+    catch(err){
+        displayError(err);
+        console.log(err);
+    }
+}
     else{
         displayError("Please enter a city name");
     
     }
-    cityInput.value = "";
 })
 
 async function getWeatherData(city){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(url);
+    if(!response.ok){
+        throw new Error("City not found");
+    }
+    const data = await response.json();
+    return data;
 
 }
 
 function displaWeatherInfo(data){
+    console.log(data)
 
 
 }
